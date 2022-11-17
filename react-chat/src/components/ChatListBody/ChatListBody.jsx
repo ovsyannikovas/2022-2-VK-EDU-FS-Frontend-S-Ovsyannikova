4dbc37afcf975fce74d5458be95b9426b7bf13b5
ovsyannikovas <s.ovsyannikova7@gmail.com>
2022-11-17 11:40:31 +0300

    Возвращена анимация

--- react-chat/src/components/ChatListBody/ChatListBody.jsx
+++ react-chat/src/components/ChatListBody/ChatListBody.jsx
@@ -4,10 +4,8 @@ import restoreDialogues from '../../utils/restoreDialogues';
 export default function ChatListBody(props) {
     return (
         <div>
-            <button>
-                <div className="new-message-btn">
-                    <i className="material-icons">edit</i>
-                </div>
+            <button className="new-message-btn pulse">
+                <i className="material-icons">edit</i>
             </button>
             <div className="message-list messages">
                 {restoreDialogues(props.props)}
