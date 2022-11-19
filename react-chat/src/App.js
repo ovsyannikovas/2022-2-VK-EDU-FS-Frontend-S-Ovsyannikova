9ad1abd22821b04dcfe10978a9e408b666a60d5d
ovsyannikovas <s.ovsyannikova7@gmail.com>
2022-11-19 14:34:30 +0300

    В App.js добавлен HashRouter

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -1,25 +1,29 @@
 import React, {Component} from "react";
 import PageChat from "./pages/PageChat/PageChat";
 import PageChatList from "./pages/PageChatList/PageChatList";
+import {HashRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            page: 'chat-list',
+            page: <Link to='/chat-list'/>,
             name: 'Дженнифер'
         }
     }
 
     render() {
         return (
-            <>
-                {
-                    this.state.page === 'chat' ?
-                        <PageChat onClick={(page) => this.setState({page: page})} name={this.state.name}/> :
-                        <PageChatList onClick={(page, name) => this.setState({page: page, name: name})}/>
-                }
-            </>
+            <Router>
+                <Routes>
+                    <Route path='/' element={
+                        <PageChatList onClick={(page, name) => this.setState({page: <Link to='/chat/id'/>, name: name})}/>
+                    }></Route>
+                    <Route path='/chat/id' element={
+                        <PageChat onClick={(page) => this.setState({page: <Link to='/chat-list'/>})} name={this.state.name}/>
+                    }></Route>
+                </Routes>
+            </Router>
         )
     }
 }
